dc591a62e4f74b20ce249dd10e56be1ef6337e65
Sanjaya <fernanda.forproject@gmail.com>
2025-03-25 11:18:26 +0700

    endpoint vendor

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Register as RegisterController;
 use App\Http\Controllers\Api\Login as LoginController;
+use App\Http\Controllers\Api\VendorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +23,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/register', [RegisterController::class, 'Register']);
 Route::post('/login', [LoginController::class, 'Login']);
+Route::middleware(['auth:sanctum'])->group(function () {
+    //vendor
+    Route::get('/vendor/all', [VendorController::class, 'index'])->middleware('isAdmin');
+    Route::post('/vendor/create', [VendorController::class, 'create'])->middleware('isAdmin');
+    Route::post('/vendor/update/{id}', [VendorController::class, 'update']);
+    Route::delete('/vendor/delete/{id}', [VendorController::class, 'destroy'])->middleware('isAdmin');
+
+});
